e41297858d921d586531d22e37d20ed43d862634
GraceCummings <cummingsge@vcu.edu>
2018-01-11 17:10:37 -0600

    thstack.py with mttbar and ratio plots

--- test/thstack.py
+++ test/thstack.py
@@ -31,11 +31,11 @@ def decideColor(filename):
         return ROOT.kCyan
 
 #Dictionary with weights
-weights = {"ttbar_ALL":"3.87","WJetsToLNu_Wpt-0To50":"205.67","WJetsToLNu_Wpt-50To100":"17.44","WJetsToLNu_Pt-100To250":"2.02","WJetsToLNu_Pt-250To400":"0.71","WJetsToLNu_Pt-400To600":"0.5\
-6","WJetsToLNu_Pt-600ToInf":"0.08","singletop_schan":"0.12","singletop_tchan_top":"8.15","singletop_tchan_antitop":"7.40","singletop_tWchan_top":"1.83","QCD_Pt_300to470_TuneCUETP8M1_13TeV_\
-pythia8":"676.64","QCD_Pt_470to600_TuneCUETP8M1_13TeV_pythia8":"58.76","QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8":"17.22","QCD_Pt_800to1000_TuneCUETP8M1_13TeV_pythia8":"2.90","QCD_Pt_100\
-0to1400_TuneCUETP8M1_13TeV_pythia8":"1.13","QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8":"0.76","QCD_Pt_1800to2400_TuneCUETP8M1_13TeV_pythia8":"0.10","QCD_Pt_2400to3200_TuneCUETP8M1_13TeV\
-_pythia8":"0.006","QCD_Pt_3200toInf_TuneCUETP8M1_13TeV_pythia8":"0.002"}
+weights = {"ttbar_ALL":"0.387","WJetsToLNu_Wpt-0To50":"20.567","WJetsToLNu_Wpt-50To100":"1.744","WJetsToLNu_Pt-100To250":"0.202","WJetsToLNu_Pt-250To400":"0.071","WJetsToLNu_Pt-400To600":"0.05\
+6","WJetsToLNu_Pt-600ToInf":"0.008","singletop_schan":"0.012","singletop_tchan_top":"0.815","singletop_tchan_antitop":"0.740","singletop_tWchan_top":".183","QCD_Pt_300to470_TuneCUETP8M1_13TeV_\
+pythia8":"67.664","QCD_Pt_470to600_TuneCUETP8M1_13TeV_pythia8":"5.876","QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8":"1.722","QCD_Pt_800to1000_TuneCUETP8M1_13TeV_pythia8":".290","QCD_Pt_100\
+0to1400_TuneCUETP8M1_13TeV_pythia8":".113","QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8":".076","QCD_Pt_1800to2400_TuneCUETP8M1_13TeV_pythia8":".010","QCD_Pt_2400to3200_TuneCUETP8M1_13TeV\
+_pythia8":".0006","QCD_Pt_3200toInf_TuneCUETP8M1_13TeV_pythia8":".0002"}
 
 #Initializes weight to 1, will stay 1 for signal
 weight = 1.0
@@ -52,6 +52,7 @@ stack_AK4Eta = ROOT.THStack("AK4Eta","AK4Eta");
 stack_AK4Phi = ROOT.THStack("AK4Phi","AK4Phi");
 stack_AK4M = ROOT.THStack("AK4M","AK4M");
 stack_AK4BDisc = ROOT.THStack("AK4BDisc","AK4BDisc");
+stack_mttbar = ROOT.THStack("mttbar","mttbar")
 
 #Define the data histograms
 h_lepPtdata   = ROOT.TH1F()
@@ -65,63 +66,66 @@ h_AK4Etadata  = ROOT.TH1F()
 h_AK4Phidata  = ROOT.TH1F()
 h_AK4Mdata    = ROOT.TH1F()
 h_AK4BDiscdata = ROOT.TH1F() 
+h_mttbardata = ROOT.TH1F()
 
 # Bkg Input files
 filelistbkg = [
-"QCD_Pt_1000to1400_TuneCUETP8M1_13TeV_pythia8_plots_ele.root",
+"QCD_Pt_1000to1400_TuneCUETP8M1_13TeV_pythia8_plots_el.root",
 "QCD_Pt_1000to1400_TuneCUETP8M1_13TeV_pythia8_plots_mu.root",
-"QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8_plots_ele.root",
+"QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8_plots_el.root",
 "QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8_plots_mu.root",
-"QCD_Pt_1800to2400_TuneCUETP8M1_13TeV_pythia8_plots_ele.root",
+"QCD_Pt_1800to2400_TuneCUETP8M1_13TeV_pythia8_plots_el.root",
 "QCD_Pt_1800to2400_TuneCUETP8M1_13TeV_pythia8_plots_mu.root",
-"QCD_Pt_2400to3200_TuneCUETP8M1_13TeV_pythia8_plots_ele.root",
+"QCD_Pt_2400to3200_TuneCUETP8M1_13TeV_pythia8_plots_el.root",
 "QCD_Pt_2400to3200_TuneCUETP8M1_13TeV_pythia8_plots_mu.root",
-"QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8_plots_ele.root",
+"QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8_plots_el.root",
 "QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8_plots_mu.root",
-"QCD_Pt_3200toInf_TuneCUETP8M1_13TeV_pythia8_plots_ele.root",
+"QCD_Pt_3200toInf_TuneCUETP8M1_13TeV_pythia8_plots_el.root",
 "QCD_Pt_3200toInf_TuneCUETP8M1_13TeV_pythia8_plots_mu.root",
-"QCD_Pt_470to600_TuneCUETP8M1_13TeV_pythia8_plots_ele.root",
+"QCD_Pt_470to600_TuneCUETP8M1_13TeV_pythia8_plots_el.root",
 "QCD_Pt_470to600_TuneCUETP8M1_13TeV_pythia8_plots_mu.root",
-"QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8_plots_ele.root",
+"QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8_plots_el.root",
 "QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8_plots_mu.root",
-"QCD_Pt_800to1000_TuneCUETP8M1_13TeV_pythia8_plots_ele.root",
+"QCD_Pt_800to1000_TuneCUETP8M1_13TeV_pythia8_plots_el.root",
 "QCD_Pt_800to1000_TuneCUETP8M1_13TeV_pythia8_plots_mu.root",
-"WJetsToLNu_Pt-100To250_plots_ele.root",
+"WJetsToLNu_Pt-100To250_plots_el.root",
 "WJetsToLNu_Pt-100To250_plots_mu.root",
-"WJetsToLNu_Pt-250To400_plots_ele.root",
+"WJetsToLNu_Pt-250To400_plots_el.root",
 "WJetsToLNu_Pt-250To400_plots_mu.root",
-"WJetsToLNu_Pt-400To600_plots_ele.root",
+"WJetsToLNu_Pt-400To600_plots_el.root",
 "WJetsToLNu_Pt-400To600_plots_mu.root",
-"WJetsToLNu_Pt-600ToInf_plots_ele.root",
+"WJetsToLNu_Pt-600ToInf_plots_el.root",
 "WJetsToLNu_Pt-600ToInf_plots_mu.root",
-"WJetsToLNu_Wpt-0To50_plots_ele.root",
+"WJetsToLNu_Wpt-0To50_plots_el.root",
 "WJetsToLNu_Wpt-0To50_plots_mu.root",
-"WJetsToLNu_Wpt-50To100_plots_ele.root",
+"WJetsToLNu_Wpt-50To100_plots_el.root",
 "WJetsToLNu_Wpt-50To100_plots_mu.root",
-"singletop_schan_plots_ele.root",
+"singletop_schan_plots_el.root",
 "singletop_schan_plots_mu.root",
-"singletop_tWchan_antitop_plots_ele.root",
+"singletop_tWchan_antitop_plots_el.root",
 "singletop_tWchan_antitop_plots_mu.root",
-"singletop_tWchan_top_plots_ele.root",
+"singletop_tWchan_top_plots_el.root",
 "singletop_tWchan_top_plots_mu.root",
-"singletop_tchan_antitop_plots_ele.root",
+"singletop_tchan_antitop_plots_el.root",
 "singletop_tchan_antitop_plots_mu.root",
-"singletop_tchan_top_plots_ele.root",
+"singletop_tchan_top_plots_el.root",
 "singletop_tchan_top_plots_mu.root",
-"ttbar_ALL_plots_ele.root",
+"ttbar_ALL_plots_el.root",
 "ttbar_ALL_plots_mu.root"]
 
 #Where we decide if we want elections and Muons
-findMe = "_ele"
+findMe = "_el"
 if options.isE:
-    findMe = "_ele"
+    findMe = "_el"
+    extra  = "_el"#yes,this is redundant. 
 elif options.isM:
     findMe = "_mu"
-    
+    extra  = "_mu"
+
 #Just to work with the list
 i=0;
 for inp in filelistbkg:
-    filelistbkg[i] = "inputfolder/CMSDAS/"+inp
+    filelistbkg[i] = "root://cmseos.fnal.gov//store/user/anovak/CMSDAS/"+inp
     i += 1
 
 #Define the name of our saved file
@@ -130,25 +134,26 @@ outname = "stacked_plots"+findMe
 # Filling with bkg hists
 for filename  in filelistbkg:
     if findMe in filename:
-        tag = filename.replace("_plots_"+findMe+".root","")
+        tag = filename[filename.rfind("/")+1:filename.rfind("_plots")]
         if tag in weights:
             weight = float(weights[tag])
     else:
         continue
     color = decideColor(filename)
-    file = ROOT.TFile(filename);
+    file = ROOT.TFile.Open(filename);
     # Reading hists from file
-    h_lepPt   = file.Get("h_lepPt");
-    h_lepEta  = file.Get("h_lepEta");
-    h_lepPhi  = file.Get("h_lepPhi");
-    h_AK8Pt   = file.Get("h_AK8Pt");
-    h_AK8Eta  = file.Get("h_AK8Eta");
-    h_AK8Phi  = file.Get("h_AK8Phi");
-    h_AK4Pt   = file.Get("h_AK4Pt");
-    h_AK4Eta  = file.Get("h_AK4Eta");
-    h_AK4Phi  = file.Get("h_AK4Phi");
-    h_AK4M    = file.Get("h_AK4M");
-    h_AK4BDisc = file.Get("h_AK4Bdisc");
+    h_lepPt   = file.Get("h_lepPt"+extra);
+    h_lepEta  = file.Get("h_lepEta"+extra);
+    h_lepPhi  = file.Get("h_lepPhi"+extra);
+    h_AK8Pt   = file.Get("h_AK8Pt"+extra);
+    h_AK8Eta  = file.Get("h_AK8Eta"+extra);
+    h_AK8Phi  = file.Get("h_AK8Phi"+extra);
+    h_AK4Pt   = file.Get("h_AK4Pt"+extra);
+    h_AK4Eta  = file.Get("h_AK4Eta"+extra);
+    h_AK4Phi  = file.Get("h_AK4Phi"+extra);
+    h_AK4M    = file.Get("h_AK4M"+extra);
+    h_AK4BDisc = file.Get("h_AK4Bdisc"+extra);
+    h_mttbar   = file.Get("h_mttbar"+extra)
 
     #Scale
     h_lepPt.Scale(weight);
@@ -162,7 +167,8 @@ for filename  in filelistbkg:
     h_AK4Phi.Scale(weight);
     h_AK4M.Scale(weight);
     h_AK4BDisc.Scale(weight);
-    
+    h_mttbar.Scale(weight)
+
     # Set color for this process
     h_lepPt.SetFillColor(color);
     h_lepEta.SetFillColor(color);
@@ -175,6 +181,7 @@ for filename  in filelistbkg:
     h_AK4Phi.SetFillColor(color);
     h_AK4M.SetFillColor(color);
     h_AK4BDisc.SetFillColor(color);
+    h_mttbar.SetFillColor(color)
 
     h_lepPt.SetLineColor(color);
     h_lepEta.SetLineColor(color);
@@ -187,6 +194,7 @@ for filename  in filelistbkg:
     h_AK4Phi.SetLineColor(color);
     h_AK4M.SetLineColor(color);
     h_AK4BDisc.SetLineColor(color);
+    h_mttbar.SetLineColor(color)
 
     # Filling stacks
     stack_lepPt.Add(copy.deepcopy(h_lepPt));
@@ -200,14 +208,15 @@ for filename  in filelistbkg:
     stack_AK4Phi.Add(copy.deepcopy(h_AK4Phi));
     stack_AK4M.Add(copy.deepcopy(h_AK4M));
     stack_AK4BDisc.Add(copy.deepcopy(h_AK4BDisc));
+    stack_mttbar.Add(copy.deepcopy(h_mttbar))
 
 # Filling with Data graphs
 if options.isE :
-    filelistdata = ["inputfolder/CMSDAS/SingleElectron_2016_All_plots_ele.root"]
+    filelistdata = ["root://cmseos.fnal.gov//store/user/anovak/CMSDAS/SingleElectron_2016_All_plots_el.root"]
 if options.isM :
-    filelistdata = ["inputfolder/CMSDAS/SingleElectron_2016_All_plots_mu.root"]
+    filelistdata = ["root://cmseos.fnal.gov//store/user/anovak/CMSDAS/singleMuon_ALL_plots_mu.root"]
 else :
-    filelistdata = ["inputfolder/CMSDAS/SingleElectron_2016_All_plots_ele.root"]
+    filelistdata = ["root://cmseos.fnal.gov//store/user/anovak/CMSDAS/SingleElectron_2016_All_plots_el.root"]
 
 for filename  in filelistdata:
     print filename
@@ -216,19 +225,21 @@ for filename  in filelistdata:
     else:
         continue
     color = ROOT.kBlack;
-    file = ROOT.TFile(filename);
+    file = ROOT.TFile.Open(filename);
     # Reading hists from file
-    h_lepPtdata   = file.Get("h_lepPt");
-    h_lepEtadata  = file.Get("h_lepEta");
-    h_lepPhidata  = file.Get("h_lepPhi");
-    h_AK8Ptdata   = file.Get("h_AK8Pt");
-    h_AK8Etadata  = file.Get("h_AK8Eta");
-    h_AK8Phidata  = file.Get("h_AK8Phi");
-    h_AK4Ptdata   = file.Get("h_AK4Pt");
-    h_AK4Etadata  = file.Get("h_AK4Eta");
-    h_AK4Phidata  = file.Get("h_AK4Phi");
-    h_AK4Mdata    = file.Get("h_AK4M");
-    h_AK4BDiscdata = file.Get("h_AK4Bdisc");
+    print "h_lepPt"+extra
+    h_lepPtdata   = file.Get("h_lepPt"+extra);
+    h_lepEtadata  = file.Get("h_lepEta"+extra);
+    h_lepPhidata  = file.Get("h_lepPhi"+extra);
+    h_AK8Ptdata   = file.Get("h_AK8Pt"+extra);
+    h_AK8Etadata  = file.Get("h_AK8Eta"+extra);
+    h_AK8Phidata  = file.Get("h_AK8Phi"+extra);
+    h_AK4Ptdata   = file.Get("h_AK4Pt"+extra);
+    h_AK4Etadata  = file.Get("h_AK4Eta"+extra);
+    h_AK4Phidata  = file.Get("h_AK4Phi"+extra);
+    h_AK4Mdata    = file.Get("h_AK4M"+extra);
+    h_AK4BDiscdata = file.Get("h_AK4Bdisc"+extra);
+    h_mttbardata = file.Get("h_mttbar"+extra)
 
     h_lepPtdata.SetMarkerStyle(20);
     h_lepEtadata.SetMarkerStyle(20);
@@ -241,6 +252,7 @@ for filename  in filelistdata:
     h_AK4Phidata.SetMarkerStyle(20);
     h_AK4Mdata.SetMarkerStyle(20);
     h_AK4BDiscdata.SetMarkerStyle(20);
+    h_mttbardata.SetMarkerStyle(20)
 
     # Set color for this process
     h_lepPtdata.SetMarkerColor(color);
@@ -254,6 +266,7 @@ for filename  in filelistdata:
     h_AK4Phidata.SetMarkerColor(color);
     h_AK4Mdata.SetMarkerColor(color);
     h_AK4BDiscdata.SetMarkerColor(color);
+    h_mttbardata.SetMarkerColor(color)
 
     h_lepPtdata.SetLineColor(color);
     h_lepEtadata.SetLineColor(color);
@@ -266,6 +279,7 @@ for filename  in filelistdata:
     h_AK4Phidata.SetLineColor(color);
     h_AK4Mdata.SetLineColor(color);
     h_AK4BDiscdata.SetLineColor(color);
+    h_mttbardata.SetLineColor(color)
 
     h_lepPtdata.SetMarkerSize(0.7);
     h_lepEtadata.SetMarkerSize(0.7);
@@ -278,7 +292,8 @@ for filename  in filelistdata:
     h_AK4Phidata.SetMarkerSize(0.7);
     h_AK4Mdata.SetMarkerSize(0.7);
     h_AK4BDiscdata.SetMarkerSize(0.7);
-    
+    h_mttbardata.SetMarkerSize(0.7)
+
 # Output file
 outfilename = outname+"_ratio.root"
 outfile = ROOT.TFile(outfilename, "RECREATE");
@@ -295,7 +310,8 @@ stack_list = [stack_lepPt,
     stack_AK4Eta,
     stack_AK4Phi,
     stack_AK4M,
-    stack_AK4BDisc]
+    stack_AK4BDisc,
+    stack_mttbar]
 data_list  = [h_lepPtdata,
     h_lepEtadata,
     h_lepPhidata,
@@ -306,7 +322,8 @@ data_list  = [h_lepPtdata,
     h_AK4Etadata,
     h_AK4Phidata,
     h_AK4Mdata,
-    h_AK4BDiscdata]
+    h_AK4BDiscdata,
+    h_mttbardata]
 
 cs1 = ROOT.TCanvas("cs1","cs1",400,500)
 pad1 = ROOT.TPad("pad1","pad1",0,0.35,1.0,1.0)
@@ -320,7 +337,7 @@ count = 0
 for stack in stack_list:
     #Plot Stacked with Data
     pad1.cd()
-    stack.Draw()
+    stack.Draw("hist")
     h_data = copy.deepcopy(data_list[count])
     print data_list[count]
     count += 1
@@ -332,11 +349,12 @@ for stack in stack_list:
     h_ratio = stack.GetHistogram()
     for hist in stack.GetHists():
         h_ratio.Add(hist)
-    h_data.Sumw2()
-    h_ratio.Sumw2()
-    h_data.Divide(h_data, h_ratio, 1, 1, "B")
-    h_data.SetStats(ROOT.kFALSE)
-    h_data.Draw("PE")
+    #h_data.Sumw2()
+    #h_ratio.Sumw2()
+    h_dataC = h_data.Clone() 
+    h_dataC.Divide(h_dataC, h_ratio, 1, 1, "B")
+    h_dataC.SetStats(ROOT.kFALSE)
+    h_dataC.Draw("PE")
     cs1.Update()
     cs1.Write(str(stack));
 
